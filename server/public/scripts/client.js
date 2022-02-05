e9c2ea81bfbfa581b1d8a582372feaf1837a22b7
Clay Webb <clay_webb@outlook.com>
2022-02-05 16:46:26 -0600

    Frontend and Backend javascript files cleaned up, GET function made

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,52 @@
+// <PAGE LOAD>------------------------------------------------------------------------------------
 $(document).ready(readyNow);
-
 function readyNow() {
     console.log('jQuery Running');
-    
+    getMath(); // This is calling the getMath function on page load
+    $('#plus').on('click', ); // This is a click listener for plus
+    $('#subtract').on('click', ); // This is a click listener for subtract
+    $('#multiply').on('click', ); // This is a click listener for multiply
+    $('#divide').on('click', ); // This is a click listener for divide
+    $('#equals').on('click', ); // This is a click listener for equals
+    $('#clear').on('click', clearCurrentEquation); // This is a click listener for clearing the inputs
+
 } // End ready Now
+
+// <PAGE LOAD>------------------------------------------------------------------------------------
+
+// <GLOBAL>--------------------------------------------------------------------------------------
+const dataToPost = {}; // Empty object to put data in to post to server
+let arrayForDom = []; // Empty array to hold the response from the server to the DOM
+// <GLOBAL>--------------------------------------------------------------------------------------
+
+
+
+// <GET LAND>------------------------------------------------------------------------------------
+function getMath(){
+    console.log('In getMath function');
+    $.ajax({
+        method: 'GET',
+        url: '/math',
+    }).then(function(response){
+        console.log('Great success', response);
+        arrayForDom = response.array;
+        renderMath(response);
+    })
+    .catch(function(response){
+        console.log('Ope no luck', response);
+    })
+} // End getAddition function
+// <GET LAND>------------------------------------------------------------------------------------
+
+
+// <POST LAND>------------------------------------------------------------------------------------
+
+// <POST LAND>------------------------------------------------------------------------------------
+
+// <CLEAR>----------------------------------------------------------------------------------
+function clearCurrentEquation(){
+    console.log('User Inputs Cleared');
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+} // End clearCurrentEquation function
+// <CLEAR>----------------------------------------------------------------------------------
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-// <SERVER SETUP>---------------------------------------------------------------
-
+// <SERVER SETUP>------------------------------------------------------------------------------
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
@@ -10,9 +9,28 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
+// <SERVER SETUP>------------------------------------------------------------------------------
 
+// <GLOBAL>--------------------------------------------------------------------------------------
+const mathHistory = []; // Declaring empty array to store created objects
+let arraySender = {}; // Declaring empty object to contain and send information to the DOM
+
+arraySender.array = mathHistory; // Created a property for arraySender and set it equal to mathHistory
+// <GLOBAL>--------------------------------------------------------------------------------------
+
+// <GET LAND>------------------------------------------------------------------------------------
+app.get('/math', function(req, res){
+    console.log('Request at /math was made', req.body);
+    res.send(arraySender);
+})
+
+// <GET LAND>------------------------------------------------------------------------------------
+
+
+// <BOTTOM>----------------------------------------------------------------------------------
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
   })
+// <BOTTOM>----------------------------------------------------------------------------------
   
   
