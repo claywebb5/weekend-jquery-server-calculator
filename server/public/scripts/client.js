a2db8e63a14133cf504278590e0c96c5f1e4e960
Clay Webb <clay_webb@outlook.com>
2022-02-06 17:07:24 -0600

    Fixed post function bugs, now able to find solution for addition

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,11 +3,8 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('jQuery Running');
     getMath(); // This is calling the getMath function on page load
-    $('#plus').on('click', theOperator); // This is a click listener for plus
-    $('#subtract').on('click', theOperator); // This is a click listener for subtract
-    $('#multiply').on('click', theOperator); // This is a click listener for multiply
-    $('#divide').on('click', theOperator); // This is a click listener for divide
-    $('#equals').on('click', ); // This is a click listener for equals
+    $('.op-button').on('click', theOperator); // This is a click listener for which operator
+    $('#equals').on('click', postSolution); // This is a click listener for equals
     $('#clear').on('click', clearCurrentEquation); // This is a click listener for clearing the inputs
 
 } // End ready Now
@@ -15,15 +12,40 @@ function readyNow() {
 // <PAGE LOAD>------------------------------------------------------------------------------------
 
 // <GLOBAL>--------------------------------------------------------------------------------------
-const dataToPost = {}; // Empty object to put data in to post to server
-let arrayForDom = []; // Empty array to hold the response from the server to the DOM
+const dataForServer = {}; // Empty object to put data in to post to server
+let arrayDataForDom = []; // Empty array to hold the response from the server to the DOM
+let mathOperator = ''; // Empty string to hold the math operator
 // <GLOBAL>--------------------------------------------------------------------------------------
 
 function theOperator(){
-    console.log('In theOperator function');
-    const mathOperator = $(this).data('math');
-    dataToPost.mathOperator = mathOperator;
+    mathOperator = $(this).data('value');
+    console.log('theOperator is:', mathOperator);
 } // End theOperator function
+
+// <POST LAND>------------------------------------------------------------------------------------
+function postSolution(){
+    console.log('Running postSolution function');
+
+    let currentSet = {
+        firstNumber: Number($('#firstNumber').val()),
+        secondNumber: Number($('#secondNumber').val()),
+        mathOperator: mathOperator
+    }
+
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: currentSet
+    }).then(function(response){
+        console.log('SUCCESS!!!', response);
+        getMath(); // Trigger GET function again
+    }).catch(function(error){
+        console.log('Error POST to server', error);
+    });
+    clearCurrentEquation();
+} // End postSolution function
+// <POST LAND>------------------------------------------------------------------------------------
+
 
 // <GET LAND>------------------------------------------------------------------------------------
 function getMath(){
@@ -33,19 +55,46 @@ function getMath(){
         url: '/math',
     }).then(function(response){
         console.log('Great success', response);
-        arrayForDom = response.array;
-        renderMath(response);
+        $('#currentAnswer').empty();
+        if(response.length > 0){
+            $('#currentAnswer').append(response[response.length-1].total);
+        }
+        renderMath(response); // ERROR ERROR ERROR ERROR ERROR ----------- ----- ----- -----
     })
-    .catch(function(response){
-        console.log('Ope no luck', response);
-    })
+    .catch(function(error){
+        console.log('Error from server', error);
+    });
 } // End getAddition function
 // <GET LAND>------------------------------------------------------------------------------------
 
+// function solution(){
+//     console.log('Running the solution function');
+//     const firstNumber = Number($('#firstNumber').val());
+//     const secondNumber = Number($('#secondNumber').val());
+//     dataForServer.firstNumber = firstNumber;
+//     dataForServer.secondNumber = secondNumber;
 
-// <POST LAND>------------------------------------------------------------------------------------
+//     postSolution(); // calling post to run when the solution function is run
+    
+//     // clearCurrentEquation(); // To clear the inputs when = is clicked
+// } // End solution function
 
-// <POST LAND>------------------------------------------------------------------------------------
+
+
+function renderMath(response){
+    $('#pastAnswers').empty(); // Clear past solutions
+    for(let data of response){
+        $('#pastAnswers').append(`
+        <div>
+            ${data.firstNumber}
+            ${data.operator}
+            ${data.secondNumber}
+            =
+            ${data.total}
+        </div>`);
+    };
+} // End renderMath function
+
 
 // <CLEAR>----------------------------------------------------------------------------------
 function clearCurrentEquation(){
--- server/server.js
+++ server/server.js
@@ -3,28 +3,74 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-
-// This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
-
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
 // <SERVER SETUP>------------------------------------------------------------------------------
 
 // <GLOBAL>--------------------------------------------------------------------------------------
 const mathHistory = []; // Declaring empty array to store created objects
-let arraySender = {}; // Declaring empty object to contain and send information to the DOM
+// let arraySender = {}; // Declaring empty object to contain and send information to the DOM
 
-arraySender.array = mathHistory; // Created a property for arraySender and set it equal to mathHistory
+// arraySender.array = mathHistory; // Created a property for arraySender and set it equal to mathHistory
 // <GLOBAL>--------------------------------------------------------------------------------------
+
+// <POST LAND>------------------------------------------------------------------------------------
+app.post('/math', (req, res) => {
+
+    let mathObject = req.body; // Empty object to push into our mathHistory array
+    console.log('The new mathObject is:', mathObject);
+
+    mathObject.total = calculate(mathObject);
+
+    mathHistory.push(mathObject);
+    res.send('It\'s working!!');
+}) // End post
+// <POST LAND>------------------------------------------------------------------------------------
+
+function calculate(mathObject) {
+    let firstNumber = mathObject.firstNumber;
+    let secondNumber = mathObject.secondNumber;
+
+    // Switch statement to do calculation
+    switch (mathObject.mathOperator){
+        case '+':
+            total = +firstNumber + +secondNumber;
+            return total;
+    }
+} // End calculate function
+
 
 // <GET LAND>------------------------------------------------------------------------------------
 app.get('/math', function(req, res){
-    console.log('Request at /math was made', req.body);
-    res.send(arraySender);
+    console.log('Request at /math was made', mathHistory);
+    res.send(mathHistory);
 })
 
 // <GET LAND>------------------------------------------------------------------------------------
+
+
+    // // Create corresponding properties for the currentMath object
+    // currentMath.firstNumber = req.body.firstNumber;
+    // currentMath.secondNumber = req.body.secondNumber;
+
+    // // if statement for plus
+    // if (req.body.theOperator == 'plus'){
+    //     currentMath.total = partsInt(req.body.firstNumber) + parseInt(req.body.secondNumber)
+    //     currentMath.operator = '+';
+    //     mathHistory.push(currentMath);
+    // }
+    // // if statement for subtract
+
+    // // if statement for multiply
+
+    // // if statement for divide
+    // console.log('arraySender is:', arraySender);
+    
+    
+
+
 
 
 // <BOTTOM>----------------------------------------------------------------------------------
